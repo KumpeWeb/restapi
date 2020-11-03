c6f4a6b0f2b29def33f655ba8b1f1e6bb48516f0
Justin Kumpe <jakumpe@kumpes.com>
2020-11-02 21:22:19 -0600

    update apns

--- kkids/POST/post-apns.php
+++ kkids/POST/post-apns.php
@@ -6,7 +6,7 @@ if($allowPost){
 	// Set Status to 202 (Accepted)
 	$statusCode = 202;
 	
-	if($isSuperUser){
+	if($isSuperuser){
 		$masterID = $_REQUEST['masterID'];
 	}
 	
@@ -24,7 +24,7 @@ if($allowPost){
 	$Action = $_REQUEST['action'];
 	$Section = $_REQUEST['section'];
 	
-	if($tool == 'register' && $isSuperUser){
+	if($tool == 'register' && $isSuperuser){
 		register_apns($token,$appName,$userID,$deviceName,$masterID);
 	}
 	
