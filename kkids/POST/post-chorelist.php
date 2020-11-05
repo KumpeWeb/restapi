cc210b5afaabdbe05e0d76eabd9bcbb21edb7eaa
Justin Kumpe <jakumpe@kumpes.com>
2020-11-05 00:22:25 -0600

    Update post-chorelist.php

--- kkids/POST/post-chorelist.php
+++ kkids/POST/post-chorelist.php
@@ -137,7 +137,7 @@ if($allowPost){
 	$choreCount = $choreCountArray['Count'];
 	
 	// kkidPushNotification($kidUsername,"Chores",NULL,NULL,$choreCount,"",NULL);
-	kkidPushNotification($kidUsername,"Chores-New","$day New Chore Added","$choreName has been added to your chore list for $day.",intval($coreCount),"default",NULL);
+	kkidPushNotification($kidUsername,"Chores-New","$day New Chore Added","$choreName has been added to your chore list for $day.","$choreCount","default",NULL);
 	
 	if($query){
 		$json = array("status" => 1, "message" => "POST Successful");
