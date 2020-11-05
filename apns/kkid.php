63f6611fcef7aa5db4356bbe86d78c81f1b214a6
Justin Kumpe <jakumpe@kumpes.com>
2020-11-04 23:09:27 -0600

    Update kkid.php

--- apns/kkid.php
+++ apns/kkid.php
@@ -3,7 +3,8 @@
 include_once('/var/www/html/kumpeapps.com/api/apns/apns.php');
 
 	function kkidPushNotification($userID,$Section,$Title,$Body,$Badge,$Sound,$Action){
-		global $conn;
+		$conn = mysqli_connect($sqlHost, $sqlUser, $sqlPass);
+		mysqli_select_db($conn, "Apps_APNs");
 		
 		$appName = 'com.kumpeapps.ios.KKid';
 
