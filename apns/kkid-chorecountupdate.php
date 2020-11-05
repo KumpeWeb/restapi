6337528d22476bd5bfc0346d3000832db46f0b3c
Justin Kumpe <jakumpe@kumpes.com>
2020-11-05 09:52:05 -0600

    Update kkid-chorecountupdate.php

--- apns/kkid-chorecountupdate.php
+++ apns/kkid-chorecountupdate.php
@@ -4,43 +4,37 @@ include_once('/var/www/html/kumpeapps.com/api/sqlConfig.php');
 include_once('/var/www/html/kumpeapps.com/api/apns/kkid.php');
 
 	$UserSQL = "
-		SELECT 
-    		userID,
-    		masterID,
-    		username
-		FROM
-    		Apps_KKid.User_Permissions
-		WHERE 1=1
-			AND isActive = 'Yes'
-    		AND isBanned = 'No'
-			AND username NOT LIKE 'Apps_%'
-    		AND username NOT LIKE 'API_%';
-	";  
+			SELECT 
+    			userID,
+    			masterID,
+    			username,
+    			(	SELECT 
+    					COUNT(*) as Count
+					FROM
+    					Apps_KKid.Chores__Today
+					WHERE 1=1
+						AND kid = Apps_KKid.User_Permissions.username
+        				AND Day != 'Weekly'
+        				AND Status = 'todo') AS choreCount
+			FROM
+    			Apps_KKid.User_Permissions
+			WHERE 1=1
+				AND isActive = 'Yes'
+    			AND isBanned = 'No'
+				AND username NOT LIKE 'Apps_%'
+    			AND username NOT LIKE 'API_%';
+		";
 	$UserQuery = mysqli_query($conn, $UserSQL) or die("Couldn't execute query. ". mysqli_error($conn)); 
 	$Users = array();
-	$UserSqlArray = mysqli_fetch_array($UserQuery)
 
 	//Run script for each user
-	while($User = $UserSqlArray)
+	while($User = mysqli_fetch_array($UserQuery))
     	$Users[] = $User;
 	foreach($Users as $UserArray){ 
 		//Set dbEXT for User
 		$username = $UserArray['username'];
-    	$sql2 = "
-			SELECT 
-    			COUNT(*) as Count
-			FROM
-    			Apps_KKid.Chores__Today
-			WHERE 1=1
-				AND kid = '$username'
-        		AND Day != 'Weekly'
-        		AND Status = 'todo';
-		";
-
-		$choreCountData = mysqli_query($conn, $sql2) or die(mysqli_error($conn));
-		$choreCountArray = mysqli_fetch_array($choreCountData);
-		$choreCount = $choreCountArray['Count'];
-	echo $username." ".$choreCount."      ";
+		$choreCount = $UserArray['choreCount'];
+	
 		//work on this function
 		kkidPushNotification($username,"Chores",NULL,NULL,intval($choreCount),"",NULL);
 	}
