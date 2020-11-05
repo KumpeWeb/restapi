b6f5048c352dbca0f0366ba393406140eb13d07d
Justin Kumpe <jakumpe@kumpes.com>
2020-11-04 23:41:29 -0600

    Update kkid.php

--- apns/kkid.php
+++ apns/kkid.php
@@ -15,7 +15,7 @@ include_once('/var/www/html/kumpeapps.com/api/apns/apns.php');
 			$useridquery = '';
 		}else if (!is_numeric($userID)){
 			$getUserID = "SELECT getUserID($userID);";
-			$userID = mysqli_query($conn, $getUserID) or die("Couldn't execute query. ". mysqli_error($conn)); 
+			$userID1 = mysqli_query($conn, $getUserID) or die("Couldn't execute query. ". mysqli_error($conn)); 
 			$useridquery = " AND userID = '$userID'";
 		}else{
 			$useridquery = " AND userID = '$userID'";
